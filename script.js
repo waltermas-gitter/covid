3fbad159cac16612d7ad1ace4ec71d17a85efac8
waltermas <walterjmas@gmail.com>
2021-01-17 11:50:05 -0300

    argentina cargado para ayer

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 window.onload = iniciar;
 
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
 async function cargarUrl(url){
 	let response = await fetch(url);
 	return response.json();
 }
 
 async function iniciar() {
-	let casos = document.getElementById("casos");
-	let json = await cargarUrl("https://api.covid19tracking.narrativa.com/api/2021-01-16/country/argentina");
+	var date = new Date();
+	date.setDate(date.getDate() - 1);
+	date_formatted = formatDate(date);
+	url = `https://api.covid19tracking.narrativa.com/api/${date_formatted}/country/argentina`
+	let json = await cargarUrl(url);
 	console.log(json);
-
+	estadisticas = json.dates[`${date_formatted}`].countries.Argentina.today_new_confirmed;
+	document.getElementById("casos").innerHTML = estadisticas;
 }
